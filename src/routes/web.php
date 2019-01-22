2c5c781101a821517761d85df53f6c86cd0a06a0
shah-shreya <46746184+shah-shreya@users.noreply.github.com>
2019-01-22 12:01:21 +0530

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['namespace' => 'App\Http\Controllers\payway'], function(){
-    Route::get('payway_status','PaywayController@getPaywayStatus');
+    Route::post('payway_status','PaywayController@getPaywayStatus');
     Route::get('paywithpayway','PaywayController@payway');
 });
