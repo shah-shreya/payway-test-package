295a1b4dd72ef44a9f44bfc7ae3585ef92600284
shah-shreya <46746184+shah-shreya@users.noreply.github.com>
2019-03-06 10:39:01 +0530

    Update payway.php

--- src/config/payway.php
+++ src/config/payway.php
@@ -7,5 +7,9 @@ return [
     'api_url' => env('PAYWAY_API_URL',''),
     
     'merchant_id' => env('PAYWAY_MERCHANT_ID',''),
+
+    'js_url' => env('PAYWAY_JS_URL',''),
+
+    'css_url' => env('PAYWAY_CSS_URL',''),
    
 ];
